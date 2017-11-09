64070e592789587f71f46f31f78b19ef4356a3e8
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-11-09 10:17:37 +0100

    good

--- src/fsa/core.clj
+++ src/fsa/core.clj
@@ -5,19 +5,48 @@
 (spec/def ::current (constantly true))
 (spec/def ::state (spec/keys :req [::current ::table]))
 
+(defn state? [x]
+  (spec/valid? ::state x))
+
+;;;;;; Helpers
+(spec/def ::dispatch-pair (spec/cat :predicate fn?
+                                    :action fn?))
+(spec/def ::dispatch-pairs (spec/* ::dispatch-pair))
+
 (defn combine2 [f g]
   (fn [state x]
     (g (f state x) x)))
 
+
+
+;;;;;; Composite ops
 (defn combine [& args]
   (reduce combine2 args))
 
+(defn eval-dispatch-pair [state x]
+  (fn [d]
+    (if ((:predicate d) x) ((:action d) state x))))
+
+(defn dispatch [& args]
+  (let [args (spec/conform ::dispatch-pairs args)]
+    (fn [state x]
+      (let [next (first
+                  (filter
+                   state?
+                   (map (eval-dispatch-pair state x) args)))]
+        (assert (state? next))
+        next))))
+
+
+;;;;;; Standard ops
 (defn go-to-state [x]
   (fn [state _]
     (assoc state ::current x)))
 
-(defn no-op [state _]
-  state)
+(defn no-op [state _] state)
+
+;;;;;;;;
+
 
 (def test-state
   {::current :whitespace
