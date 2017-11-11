b2991fafdee5e819100f9fbe55b311c7f100c906
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-11-11 18:26:31 +0100

    great

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fsa "0.1.0-SNAPSHOT"
+(defproject bluebell/fsa "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
